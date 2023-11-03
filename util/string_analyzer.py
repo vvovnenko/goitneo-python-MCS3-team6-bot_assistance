ead2faf4882c77cd7ee922cae0ae01056a41ad44
Mykola Podopryhora <Mykola.Podopryhora@harmonicinc.com>
2023-11-03 16:25:38 +0200

    Improve smart commands guess

--- util/string_analyzer.py
+++ util/string_analyzer.py
@@ -1,7 +1,21 @@
+import re
+
+
+def sanitize_args(func):
+    def wrapper(arg1, arg2):
+        arg1 = sanitize_args(arg1)
+        arg2 = sanitize_args(arg2)
+        return func(arg1, arg2)
+    return wrapper
+
+@sanitize_args
 def get_similarity_score(input: str, guess: str):
+
     score = 0
 
-    if guess.startswith(input):
+    if guess.startswith(input) or input.startswith(guess):
+        score += 5
+    elif count_similar_characters(input, guess) >= max(len(input), len(guess)) - 1:
         score += 5
 
     if score == 0 and (len(input) < len(guess)/3):
@@ -10,15 +24,8 @@ def get_similarity_score(input: str, guess: str):
     if is_anagram(input, guess):
         score += 3
 
-    if (are_substrings(input, guess)):
+    if are_substrings(input, guess):
         score += 1
-
-    len_diff = abs(len(input) - len(guess))
-
-    # additional score for less diff in length
-    if score > 0:
-        if len_diff <= 3:
-            score += 4 - len_diff
 
     return score
 
@@ -29,3 +36,18 @@ def is_anagram(str1, str2):
 
 def are_substrings(str1, str2):
     return str1 in str2 or str2 in str1
+
+
+def count_similar_characters(str1, str2):
+    count = 0
+
+    for char1 in str2:
+        if char1 in str1:
+            count += 1
+            str1.replace(char1, '', 1)
+
+    return count
+
+
+def sanitize_args(input_string):
+    return re.sub(r'[^a-zA-Z]', '', input_string)
